dd165d7b33f517e829656cdc7f9a35c8a3d5e82e
刘世伟 <liushiwei@starlinke.com>
2026-09-01 11:40:50 +0800

    Configure Supabase client

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // Supabase 的 Project URL 和 publishable key 都是前端公开配置，不是密码。
 // 绝对不要把 service_role key 或 secret key 写入这个文件。
-export const SUPABASE_URL = '';
-export const SUPABASE_PUBLISHABLE_KEY = '';
-
+export const SUPABASE_URL = 'https://xvplgypphhfuxidkebok.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_HkML_7fw8xXGLkq7YWuouA_ofl4KdKB';
